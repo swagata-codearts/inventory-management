56e14726ecc768596a0ea9815dbda0ca7c6a5d02
Dipan-25 <dipan.codearts@gmail.com>
2022-07-01 14:55:07 +0530

    weather api call

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  getWeatherData(): Observable<any>{
+    let lat = 22.717743;
+    let lon =  88.478187;
+    let apiId = '713b46f6d6e8382350087f18c2eeb0d1';
+    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId};
+
+    return this.http.get(queryString);
+
+  }
   
 }

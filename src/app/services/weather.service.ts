523ca14e49b9b0c648d25712a24b243e56e9397a
Dipan-25 <dipan.codearts@gmail.com>
2022-07-04 15:24:28 +0530

    new api id added

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,8 +12,8 @@ export class WeatherService {
   getWeatherData(): Observable<any>{
     let lat = 22.717743;
     let lon =  88.478187;
-    let apiId = '713b46f6d6e8382350087f18c2eeb0d1';
-    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId};
+    let apiId = '11b74bd067504f82a42172040221606';
+    let queryString = 'http://api.weatherapi.com/v1/forecast.json?key=${apiId}&q=${city}&days=1&aqi=no&alerts=no';
 
     return this.http.get(queryString);
 

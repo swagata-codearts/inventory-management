43de84844ea3a3bb51212a21c93a8f9227c02746
Swagata-codearts <swagata.codearts@gmail.com>
2022-07-04 15:37:07 +0530

    Weather services change

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,7 +13,7 @@ export class WeatherService {
     let lat = 22.717743;
     let lon =  88.478187;
     let apiId = '713b46f6d6e8382350087f18c2eeb0d1';
-    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId};
+    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&exclude=minutely,alerts&appid=' + apiId;
 
     return this.http.get(queryString);
 
--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherPage implements OnInit {
 
-  constructor() { }
+  public res;
+
+  constructor(
+    private weatherAPI: WeatherService
+    ) { }
 
   ngOnInit() {
+    this.getWeatherData();
   }
 
+  getWeatherData()  {
+    this.weatherAPI.getWeatherData().subscribe((response) => {
+      this.res = response;
+      console.log(this.res);
+    })
+  }
 }
